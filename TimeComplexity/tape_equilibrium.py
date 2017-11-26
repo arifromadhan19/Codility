6b99e5fe0d3132a9c739ea99ca26e1228fb18b0f
arifromadhan19 <arifromadhan19@gmail.com>
2017-11-26 16:46:23 +0700

    add solution codility

--- TimeComplexity/tape_equilibrium.py
+++ TimeComplexity/tape_equilibrium.py
@@ -1,7 +1,8 @@
 # you can write to stdout for debugging purposes, e.g.
 # print("this is a debug message")
 
-def solution(A):
+#FISRT WAY BUT SCORE = 50
+def solution1(A):
     # write your code in Python 3.6
     temp = []
     for i in range(1,len(A)):
@@ -10,3 +11,25 @@ def solution(A):
         temp.append(abs(sum_head- sum_tail))
     return min(temp)
     pass
+
+#SECOND WAY, SCORE = 100
+def solution2(A):
+    head = A[0]
+    tail = sum(A[1:])
+    min_dif = abs(head - tail)
+
+    for index in range(1, len(A) - 1):
+        head += A[index]
+        tail -= A[index]
+        if abs(head - tail) < min_dif:
+            min_dif = abs(head - tail)
+
+    return min_dif
+    pass
+
+
+A = [3, 1, 2, 4, 3]
+print(solution1(A))
+print(solution2(A))
+
+
